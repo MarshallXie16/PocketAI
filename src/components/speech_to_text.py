624797768e3c4f2e7978126285c1a3dabee14751
MarshallXie16 <marshallxie16@gmail.com>
2024-06-13 11:28:19 -0700

    removed STT and TTS temporarily

--- src/components/speech_to_text.py
+++ src/components/speech_to_text.py
@@ -1,78 +1,78 @@
-import speech_recognition as sr
-from AudioRecorder import AudioRecorder
-import keyboard
-from src.utils.openai_client import client
-from src.utils.audio_record import start_recording_listener
+# import speech_recognition as sr
+# from AudioRecorder import AudioRecorder
+# import keyboard
+# from src.utils.openai_client import client
+# from src.utils.audio_record import start_recording_listener
 
-'''
-Currently supports
-- whisper transcription
-- googleSTT real-time speech recognition (deprecated)
+# '''
+# Currently supports
+# - whisper transcription
+# - googleSTT real-time speech recognition (deprecated)
 
-Future updates
-- ability to interrupt/type while speaking
-'''
-
-
-# represents a generic speech-to-text service
-class SpeechToText:
-
-    def __init__(self):
-        # print('Transcription by whisper')
-        # print('Transcription by google')
-        self.speech_engine = Whisper()
-
-    # calls the listen method of the transcription tool
-    def listen(self):
-        transcript = self.speech_engine.listen()
-        return transcript
+# Future updates
+# - ability to interrupt/type while speaking
+# '''
 
 
-# uses google speech recognizer to record and convert audio to text
-class GoogleSTT:
+# # represents a generic speech-to-text service
+# class SpeechToText:
 
-    def __init__(self):
-        self.recognizer = sr.Recognizer()
+#     def __init__(self):
+#         # print('Transcription by whisper')
+#         # print('Transcription by google')
+#         self.speech_engine = Whisper()
 
-    # listens for too long (???)
-    def listen(self):
-        # use the microphone to record audio
-        with sr.Microphone() as source:
-            print("Recording...")
-            audio = self.recognizer.listen(source, phrase_time_limit=15)
-        try:
-            transcript = self.recognizer.recognize_google(audio)
-        # speech is unintelligible
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-            transcript = "E1"
-        # API is unreachable
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-            transcript = "E2"
-
-        return transcript
+#     # calls the listen method of the transcription tool
+#     def listen(self):
+#         transcript = self.speech_engine.listen()
+#         return transcript
 
 
-class Whisper:
-    def __init__(self):
-        pass
+# # uses google speech recognizer to record and convert audio to text
+# class GoogleSTT:
 
-    # transcribes a given audio clip; record audio if using text interface
-    def listen(self):
-        # only for text interface
-        # start_recording_listener()
-        try:
-            audio_file = open("../prototypes/recording.wav", "rb")
-            transcript = client.audio.transcriptions.create(
-                model='whisper-1',
-                file=audio_file
-            ).text
-        except Exception as e:
-            print(f"Could not transcribe file: {e}")
-            transcript = 'E2'
+#     def __init__(self):
+#         self.recognizer = sr.Recognizer()
 
-        return transcript
+#     # listens for too long (???)
+#     def listen(self):
+#         # use the microphone to record audio
+#         with sr.Microphone() as source:
+#             print("Recording...")
+#             audio = self.recognizer.listen(source, phrase_time_limit=15)
+#         try:
+#             transcript = self.recognizer.recognize_google(audio)
+#         # speech is unintelligible
+#         except sr.UnknownValueError:
+#             print("Google Speech Recognition could not understand audio")
+#             transcript = "E1"
+#         # API is unreachable
+#         except sr.RequestError as e:
+#             print("Could not request results from Google Speech Recognition service; {0}".format(e))
+#             transcript = "E2"
+
+#         return transcript
 
 
-speech_to_text = SpeechToText()
+# class Whisper:
+#     def __init__(self):
+#         pass
+
+#     # transcribes a given audio clip; record audio if using text interface
+#     def listen(self):
+#         # only for text interface
+#         # start_recording_listener()
+#         try:
+#             audio_file = open("../prototypes/recording.wav", "rb")
+#             transcript = client.audio.transcriptions.create(
+#                 model='whisper-1',
+#                 file=audio_file
+#             ).text
+#         except Exception as e:
+#             print(f"Could not transcribe file: {e}")
+#             transcript = 'E2'
+
+#         return transcript
+
+
+# speech_to_text = SpeechToText()
