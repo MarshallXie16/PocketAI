5a49485483de1200f5ca074dfaaf11da2662a75c
MarshallXie16 <marshallxie16@gmail.com>
2024-06-18 15:29:35 -0700

    extended models to fix truncation error

--- src/models/google_user.py
+++ src/models/google_user.py
@@ -4,7 +4,7 @@ from src.utils.extensions import db, migrate
 
 class GoogleUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    google_id = db.Column(db.String(128), db.ForeignKey('user.google_id'))
+    google_id = db.Column(db.String(256), db.ForeignKey('user.google_id'))
     access_token = db.Column(db.String(512), nullable=True)
     refresh_token = db.Column(db.String(512), nullable=True)
     token_expire_at = db.Column(db.DateTime, nullable=True)
